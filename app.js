a91aa66497257731b4baff132b969b1c11689d5e
Yaeji Shin <yshin123@umd.edu>
2021-02-19 18:04:29 -0500

    change paint brush size

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const canvas = document.getElementById("jsCanvas");
 const context = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
 canvas.width = 700;
 canvas.height = 700;
+
 context.strokeStyle = "black";
-context.luneWidth = 2.5;
+context.lineWidth = 5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
    painting = false;
@@ -44,6 +48,22 @@ function handleColorClick(event) {
    context.strokeStyle = color;
 }
 
+function handleRangeChange(event) {
+   const size = event.target.value;
+   context.lineWidth = size;
+   // console.log(event.target.value);
+}
+
+function handleModeClick(event) {
+   if (filling === true) {
+      filling = false;
+      mode.innerText = "Fill";
+   } else {
+      filling = true;
+      mode.innerText = "Paint";
+   }
+}
+
 if (canvas) {
    canvas.addEventListener("mousemove", onMouseMove);
    canvas.addEventListener("mousedown", startPainting);
@@ -53,3 +73,11 @@ if (canvas) {
 
 Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
 // console.log(Array.from(colors));
+
+if (range) {
+   range.addEventListener("input", handleRangeChange);
+}
+
+if (mode) {
+   mode.addEventListener("click", handleModeClick);
+}
